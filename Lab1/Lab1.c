1a3a54c7482e3bb88d6f06a58e26612060f598f1
Joshua Huynh <flashcastuser@gmail.com>
2020-02-29 23:42:10 -0800

    Lab 1 complete.  Possibly errors with the outputs of system time.

--- Lab1/Lab1.c
+++ Lab1/Lab1.c
@@ -8,56 +8,61 @@
 #include <sys/wait.h> // For waitpid
 #include <sys/times.h> //For times()
 
-void getInfo(pid_t fork_val,int status);
+void getInfo(pid_t fork_val,int status); //Both the parent and the child use this function to report its process information.
+void getTimes(pid_t fork_val,struct tms buf); //Finds and print the user time and system time for both the parent and child processes.  Also prints time(NULL) afterwards.
 
 int main()
 {
     struct tms buf;
-    times(&buf);
-    int status;
-    printf("START: %ld\n",time(NULL));
+    times(&buf); //This function gives the user time and system time for each the parent process and child process
 
-    pid_t fork_val = fork();
-    if(fork_val == -1)
-    {
+    int status;
+    printf("START: %ld\n",time(NULL)); //prints the number of seconds since the Epoch (00:00:00 UTC, January 1, 1970).
+
+    pid_t fork_val = fork(); // fork() creates a child process.  On the parent process, the child's process ID is returned.  On the child process, a 0 is returned.
+
+    if(fork_val == -1){ // If fork() returns -1, then a failure to create a child object has occurred
         printf("Failed to create child process.");
         return 0;
     }
 
-    waitpid(fork_val,&status,0);
-
-    if(fork_val > 0){
-        printf("PPID: %d,  ", getpid());
-        printf("PID: %d,  \n", fork_val);
+    if(fork_val > 0){ // If a PID was assigned to fork_val
+        printf("PPID: %d,  ", getpid()); //Print the parent process's ID
+        printf("PID: %d,  \n", fork_val); //Print the child's process's ID
     }
+
+    waitpid(fork_val,&status,0); // The parent process stops right here, and waits until the child process has finished running.  The parent then continues.
 
     getInfo(fork_val,status);
-
-    if(fork_val > 0){
-        printf("USER:  %ld,  ", buf.tms_utime);
-        printf(" SYS: %ld\n", buf.tms_stime);
-        printf("CUSER: %ld,  ", buf.tms_cutime);
-        printf("CSYS: %ld,\n",buf.tms_cstime);
-        printf("STOP: %ld\n",time(NULL));
-    }
+    getTimes(fork_val,buf);
 
     return 0;
 }
 
-void getInfo(pid_t fork_val, int status)
+void getInfo(pid_t fork_val, int status) //Both the parent and the child use this function to report its process information.
 {
-
-
     if(fork_val > 0){
         printf("PPID: %d,  ", getppid());
         printf("PID: %d,  ", getpid());
-        printf("CPID: %d,", fork_val);
+        printf("CPID: %d,  ", fork_val);
         printf("RETVAL: %d\n",status);
     }
 
     return;
 
 
+}
+void getTimes(pid_t fork_val,struct tms buf) //Finds and print the user time and system time for both the parent and child processes.  Also prints time(NULL) afterwards.
+{
+
+    if(fork_val > 0){ //If a PID was assigned to fork_val, then print the user time and system time for each processes.
+        printf("USER:  %ld,  ", buf.tms_utime);
+        printf(" SYS: %ld\n", buf.tms_stime);
+        printf("CUSER: %ld,  ", buf.tms_cutime);
+        printf("CSYS: %ld\n", buf.tms_cstime);
+        printf("STOP: %ld\n",time(NULL));
+    }
+    return;
 }
 
 
