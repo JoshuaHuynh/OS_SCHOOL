43fbd21f316227c11c3b514840f9ea58dc2461a1
Joshua Huynh <flashcastuser@gmail.com>
2020-05-18 03:10:33 -0700

    Lab 7. Not finished. But I am about to redo it because this way is too complicated

--- Lab7/matrix_add.c
+++ Lab7/matrix_add.c
@@ -8,22 +8,25 @@
 #include <sys/wait.h>
 #include <sys/times.h>
 #include <aio.h>
+#include <ctype.h>
 
-
+struct aiocb aio;
+char one[1];
+int offNum = 0;
 int main(int argc, char* const argv[])
 {
 
-    int status;
     int theSign = rand() % 2;
     int oneOrZero = rand() % 2;
     int block_size = 0;
     int blocks = 0;
     int size1 = 0;
-    int scalar = (rand() % 100) + oneOrZero;
+    int scalar = (rand() % 100) + oneOrZero; //Scalar can be any number in the range -100 to 100.
     int x;
     int y;
-    struct aiocb aio;
-    aio.aio_fildes = argv[3];
+    char number[200];
+    time_t startTime,endTime,timeFinish;
+
 
     if (theSign == 0)
         scalar = scalar * 1;
@@ -36,19 +39,70 @@ int main(int argc, char* const argv[])
     long num2 = strtol(argv[2],NULL,10); //Changes argv[2] from string to number.
     blocks = (int)num2;
 
+    int elements[size1][size1];
+
+    int totalBlocks = blocks * blocks;
+
     block_size = size1/blocks;
 
 
-    printf("START: %ld\n",time(NULL)); //prints the number of seconds since the Epoch (00:00:00 UTC, January 1, 1970).
+    char buf1[block_size * 2];
+    memset(&aio,0,sizeof(struct aio));
+    aio.aio_fildes = STDIN_FILENO;
+    aio.aio_buf = one;
+    aio.aio_nbytes = 1;
+    aio.aio_offset = 0;
+
+
+
+    startTime = time(NULL); //Get the current time.
+
 
     for(x = 1; x < blocks; x++){
         for(y = 1; y < blocks; y++){
 
+
+
         }
     }
-    printf("STOP: %ld\n",time(NULL));
+    endTime = time(NULL);
+
+    timeFinish = endTime - startTime;
+
+    fprintf(stderr,"%ld",timeFinish);
 
     return 0;
+}
+
+void newLine()
+{
+    while(aio_read(&aio) == 0){
+        if(one[0] == '\n'){
+            aio.aio_offset = offNum + 1;//Stops at beginning of new line.
+            aio_return(&aio);
+            break;
+        }
+        aio.aio_offset = offNum++;
+        aio_return(&aio);
+
+    }
+}
+
+void nextBlock(int x, int block_size)
+{
+    int stop = 0;
+    while(aio_read(&aio) == 0){
+        if(one[0] == ' ' || one[0] == '\n'){
+            stop++;
+        }
+        if(stop == block_size){//Stopped at the beginning of the number to read in the next block.
+            aio_return(&aio);
+            break;
+
+        }
+        aio.aio_offset = offNum++;
+        aio_return(&aio);
+    }
 }
 
 
