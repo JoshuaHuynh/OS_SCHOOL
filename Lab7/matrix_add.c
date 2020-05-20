fccffda775748f22f84b94da9a886f60546518e6
Joshua Huynh <flashcastuser@gmail.com>
2020-05-20 00:52:34 -0700

    Lab 7. Added to the READ.ME file. Also added comments to matrix_add.c

--- Lab7/matrix_add.c
+++ Lab7/matrix_add.c
@@ -20,30 +20,30 @@ char one[5];
 int rows;
 int cols;
 
-void matrix_add(int curBlock[rows][cols],int block_size,int scalar);
+void matrix_add(int curBlock[rows][cols],int block_size,int scalar); //This function adds a scalar to the block passed to this function.
 
 int main(int argc, char* const argv[])
 {
     (void)argc;
     srand(time(NULL));
-    int theSign = rand() % 2;
-    int oneOrZero = rand() % 2;
+    int theSign = rand() % 2; //This variable is to determine whether the integer in the matrix is to be negative or posiitive.
+    int oneOrZero = rand() % 2; //Since the way I create random numbers only generate numbers between 0 to 99, I need to add 1 to be able to reach 100.
     int block_size = 0;
     int blocks = 0;
-    int size1 = 0;
+    int size1 = 0; //Size of matrix, as in size by size.
     int scalar = (rand() % 100) + oneOrZero; //Scalar can be any number in the range -100 to 100.
     int x,y,i,j;
-    int curSize = 0;
+    int curSize = 0; //The current number of elements in the block. Also, used to determining when to go to a new line in the matrix.
     size_t n;
-    char current[200];
-    char one[5];
+    char current[200]; //The current number or element that the while-loop is on.
+    char one[5]; //Used to hold one character. (The loop reads on character at a time, so this char array holds the current character the loop is on.
     time_t startTime,endTime,timeFinish;
-    int offNum = 0;
-    int stop = 0;
+    int offNum = 0; //Used for iterating or traversing through the file being read from.  This is the offset number.
+    int stop = 0; //Indicates when the loop finished forming a block.
     int blockRow = 0, blockCol = 0;
-    char numPrint[200] = "";
+    char numPrint[200] = ""; //Used to hold a string or number to be printed on a file.
     long temp;
-    off_t offW = 0;
+    off_t offW = 0; //Offset for aio_write.
 
 
     if (theSign == 0)
@@ -77,12 +77,12 @@ int main(int argc, char* const argv[])
 
     startTime = time(NULL); //Get the current time.
 
-    for(x = 0; x < blocks; x++){
+    for(x = 0; x < blocks; x++){ //These two for-loops iterate or traverse through each block in the matrix read from the file.
         for(y = 0; y < blocks; y++){
             strcpy(current,"");
-            while(aio_read(&aio) == 0){ //This loop breaks after current block is filled up.
+            while(aio_read(&aio) == 0){ //This loop reads the matrix from the file. Inside this loop, the current block is being formed by being put into a two dimensional array.
                 aio_return(&aio);
-                while(aio_error(&aio) == EINPROGRESS){
+                while(aio_error(&aio) == EINPROGRESS){ //Waiting until the read request is done.
 
                 }
                 if (strcmp(one,"0") == 0 || strcmp(one,"1") == 0|| strcmp(one,"2") == 0|| strcmp(one,"3") == 0|| strcmp(one,"4") == 0|| strcmp(one,"5") == 0|| strcmp(one,"6") == 0|| strcmp(one,"7") == 0|| strcmp(one,"8") == 0|| strcmp(one,"9") == 0|| strcmp(one,"-") == 0){
@@ -104,7 +104,7 @@ int main(int argc, char* const argv[])
 
                 offNum++;
                 aio.aio_offset = (off_t)offNum;
-                //printf("%s",one);
+
                 if(stop == (block_size *block_size)){
                     blockRow = 0;
                     blockCol = 0;
@@ -116,7 +116,7 @@ int main(int argc, char* const argv[])
 
             matrix_add(curBlock,block_size,scalar);
 
-            for(i = 0; i < block_size; i++){
+            for(i = 0; i < block_size; i++){ //These two for-loops ultimately prints the current block onto a file.
                 for(j = 0; j < block_size; j++){
                     curSize++;
                     if(curSize == size1){
@@ -151,7 +151,7 @@ int main(int argc, char* const argv[])
 
     timeFinish = endTime - startTime;
 
-    fprintf(stderr,"Amount of time to perform operation: %ld\n",timeFinish);
+    fprintf(stderr,"Amount of time to perform operation: %ld\n",timeFinish); //Emits the time taken to complete the process.
     return 0;
 }
 
